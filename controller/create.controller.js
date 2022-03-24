d25bab7f0a439b3f85edb37fa7ed8d5090aa87e1
ibrahim kehinde <ibrahimkenny17@gmail.com>
2022-03-24 08:48:32 -0700

    Leadway Interview task

--- controller/create.controller.js
+++ controller/create.controller.js
@@ -1,5 +1,6 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const crypto = require("crypto")
 const User = require("../models/user.model");
 const tokenSecretKey = require("crypto").randomBytes(32).toString("hex");
 
@@ -55,9 +56,39 @@ module.exports = {
         const token = jwt.sign(data, tokenSecretKey, {
             expiresIn: tokenExpirationTime,
         })
+        // console.log(token)
         return res.status(200).json({result: "Welcome to Leadway Assurance", token})
         } catch (err) {
             res.status(400).json({message: err.message});
+        }
+    },
+    reset_password: async (req, res) =>{
+        
+
+        try{
+            const {userId, resetToken, password} = req.body;
+            const user = await User.findById(userId)
+            console.log(user)
+            if (!user) return res.status(400).json({result: "User not Found"});
+            
+            const token = await Token.findOne({userId});
+            if(!token) return res.status(400).json({result: "Token not found"})
+
+            const isValid = await bcrypt.compare(resetToken, token.token);
+            if(isValid) return res.status(400).json({result: "Token is not valid"})
+
+            const salt = await bcrypt.genSalt(10);
+            const hashedPassword = await bcrypt.hash(password, salt);
+
+            user.password = hashedPassword;
+            await user.save();
+
+            // token deletion
+            token.remove();
+             
+            return res.status(200).json({result: "Password reset successfully!"})
+        }catch (error) {
+            return res.status(400).json({message: "error.message"})
         }
     }
     
--- routes/create.js
+++ routes/create.js
@@ -5,4 +5,5 @@ const createCtrl = require("../controller/create.controller");
 // GET home page
 router.post("/register", createCtrl.register);
 router.post("/login", createCtrl.login);
+router.post("/reset", createCtrl.reset_password);
 module.exports = router;
